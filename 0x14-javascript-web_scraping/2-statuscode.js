4736d436e8eb5858a06e8410932e892ce4891cc0
“Gilbert <>
2023-04-25 03:19:32 -0700

    Task 2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let request = require('request');
+let URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
