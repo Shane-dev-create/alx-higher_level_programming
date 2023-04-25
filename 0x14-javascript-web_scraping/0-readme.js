636a592bb38b12189f7ea958297782ec0ce341a5
“Gilbert <>
2023-04-25 03:15:14 -0700

    Task 0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let fs = require('fs');
+let file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
