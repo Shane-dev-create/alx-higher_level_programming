b4543030d0483d7dff35773336a18b6b7c74284d
“Gilbert <>
2023-04-25 05:44:06 -0700

    fiximg error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], body, function (error) {
+      if (error) { console.log(error); }
+    });
   }
 });
