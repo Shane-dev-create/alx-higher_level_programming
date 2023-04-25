35d3e1a7286680ae77e56a376484df4644e1c931
“Gilbert <>
2023-04-25 05:28:44 -0700

    fix error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,16 +5,19 @@ const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    let results = JSON.parse(body).results;
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
     let count = 0;
-    for (let i in results) {
-      for (let chars of results[i].characters) {
-        if (chars.search('/18/') > 0) { count += 1; }
+    for (const i in films) {
+      const chars = films[i].characters;
+      for (const j in chars) {
+        if (chars[j].includes('18')) {
+          count++;
+        }
       }
     }
     console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
-
-
