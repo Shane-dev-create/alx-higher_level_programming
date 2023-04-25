dfeae4a7acf46e803a29b29aedfe2d59dd2c48f4
“Gilbert <>
2023-04-25 06:00:29 -0700

    Fixing erros

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,19 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else if (response.statusCode === 200) {
     const completed = {};
-    const todos = JSON.parse(body);
-    for (const i in todos) {
-      const todo = todoss[i];
-      if (todo.completed === true) {
-        if (completed[todo.userId] === undefined) {
-          completed[todo.userId] = 1;
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          completed[todo.userId]++;
+          completed[task.userId]++;
         }
       }
     }
